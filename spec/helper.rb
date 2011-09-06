df321f8f7994aa4dc7b2ba7daa515833002b1714
Maurício Szabo <mauricio.szabo@gmail.com>
2011-09-06 18:30:52 -0300

    Atualizado Spec Helper.

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,8 +1,8 @@
 require 'env_johnson'
+require "env_johnson/matchers"
 require "rubygems"
 require "nokogiri"
 
-RSpec = Spec if Object.const_defined? :Spec
 RSpec::Matchers.define :have_tag do |css_selector| 
   match do |body| 
     not Nokogiri.parse(body).css(css_selector).empty? 
