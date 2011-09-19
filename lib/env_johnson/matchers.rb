9b5da2cb71616dcf0d1834694380cd5b284c8c72
Mauricio Szabo <mauricio.szabo@gmail.com>
2011-09-18 23:14:41 -0300

    Melhorado o matcher do Replace Inner HTML.

--- lib/env_johnson/matchers.rb
+++ lib/env_johnson/matchers.rb
@@ -1,16 +1,65 @@
 require "nokogiri"
 
+#TODO: Move to other file and SPEC it.
+class SpecEnvJohnson < EnvJohnson
+  def initialize(js_code, *javascripts)
+    super
+    @code = js_code
+  end
+
+  def inspect
+    "\"#@code\""
+  end
+end
+
 RSpec = Spec if Object.const_defined? :Spec
 
-RSpec::Matchers.define :replace_inner_html_of do |element_id, params|
-  match do |js|
-    env = EnvJohnson.new js, :elements => [element_id]
-    before = Nokogiri.parse env.body_before
-    after = Nokogiri.parse env.body
-    before = before.css("##{element_id}").inner_html
-    after = after.css("##{element_id}").inner_html
-    
+module Matchers
+  class ReplaceInner
+    def initialize(element_id, params={})
+      @element_id = element_id
+      @params = params
+    end
 
-    before != after
+    def matches?(js)
+      @js = js
+      before = Nokogiri.parse @js.body_before
+      after = Nokogiri.parse @js.body
+      before = before.css("##{@element_id}").inner_html
+      after = after.css("##{@element_id}").inner_html
+      
+      conditions = [before != after]
+      conditions << (after == @params[:with]) if @params[:with]
+      conditions.all?
+    end
+
+    def with(string)
+      ReplaceInner.new @element_id, @params.merge(:with => string)
+    end
+
+    def failure_message
+      "Expected #{@js.inspect} #{rest}"
+    end
+    def negative_failure_message
+      "Expected #{@js.inspect} not to replace element '#{@element_id}'"
+    end
+
+    def rest
+      rest = "to replace element '#{@element_id}'"
+      rest << " with #{@params[:with]}" if @params[:with]
+      return rest
+    end
+    private :rest
+  end
+end
+
+module RSpec::Matchers
+  def js_for(string, params={})
+    js = SpecEnvJohnson.new string, params
+    yield js
+  end
+
+  def replace_inner_html_of(element_id)
+    Matchers::ReplaceInner.new(element_id)
   end
 end
--- spec/env_johnson/matchers_spec.rb
+++ spec/env_johnson/matchers_spec.rb
@@ -3,14 +3,17 @@ require "spec/helper"
 describe 'Custom matchers' do
   it 'should match when replacing text' do
     js_code = "document.getElementById('example').innerHTML = 'Changed'"
-    js_code.should replace_inner_html_of(:example)
-    js_code.should_not replace_inner_html_of(:example2)
+    js_for js_code do |js|
+      js.should replace_inner_html_of(:example)
+      js.should_not replace_inner_html_of(:example2)
+    end
   end
 
   it 'should match replaced text' do
-    pending
     js_code = "document.getElementById('example').innerHTML = 'Changed'"
-    js_code.should replace_inner_html_of(:example, :with => 'Changed')
-    js_code.should_not replace_inner_html_of(:example, :with => 'Original')
+    js_for js_code do |js|
+      js.should replace_inner_html_of(:example).with('Changed')
+      js.should_not replace_inner_html_of(:example).with('Original')
+    end
   end
 end
