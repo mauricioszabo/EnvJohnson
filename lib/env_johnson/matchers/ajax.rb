6cf8007b903fbfd2335e8f4279a1b9d6f1af7902
Maurício Szabo <mauricio.szabo@gmail.com>
2011-09-29 19:49:56 -0300

    Chamada remota quase pronta.

--- lib/env_johnson/matchers/ajax.rb
+++ lib/env_johnson/matchers/ajax.rb
@@ -8,19 +8,41 @@ module Matchers
       Matchers::Ajax.new @params.merge(:to => uri)
     end
 
+    def with_parameters(params)
+      old_params = @params[:params] || {}
+      Matchers::Ajax.new @params.merge(:params => old_params.merge(params))
+    end
+    alias :with_params :with_parameters
+
     def matches?(js)
       @js = js
       conditions = [@js.remote_calls.size > 1]
-      conditions << to_remote if @params[:to]
+      conditions << to_remote? if @params[:to]
+      conditions << with_parameters? if @params[:params]
       conditions.all?
     end
 
-    def to_remote
-      with_trailing = @params[:to] === @js.remote_calls[-1]
-      without_trailing = @params[:to] === @js.remote_calls[-1][1..-1]
+    def to_remote?
+      call = @js.remote_calls[-1].gsub(/\?.*/, "")
+      with_trailing = @params[:to] === call
+      without_trailing = @params[:to] === call[1..-1]
       without_trailing or with_trailing
     end
-    private :to_remote
+    private :to_remote?
+
+    def with_parameters?
+      uri_params = @js.remote_calls[-1].gsub(/.*?\?/, "")
+      uri_params = uri_params.split("&").map do |x| 
+        k, v = x.split("=")
+        [k, URI.unescape(v.to_s)]
+      end
+      params = @params[:params]
+      params.all? do |key, value|
+        key, value = key.to_s, value.to_s
+        uri_params.include?([key, value])
+      end
+    end
+    private :with_parameters?
 
     def failure_message
       "Expected #@js to #{rest}"
@@ -33,6 +55,7 @@ module Matchers
     def rest
       "be an ajax call".tap do |m|
         m << " to #{@params[:to].inspect}" if @params[:to]
+        m << " with parameters #{@params[:params].inspect}" if @params[:params]
       end
     end
     private :rest
--- spec/env_johnson/matchers/ajax_spec.rb
+++ spec/env_johnson/matchers/ajax_spec.rb
@@ -4,7 +4,7 @@ describe Matchers::Ajax do
   let(:ajax_call) { 
     <<-EOF
       xmlhttp=new XMLHttpRequest();
-      xmlhttp.open("GET","example",true);
+      xmlhttp.open("GET","example?id=20&ba=foo%20bar",true);
       xmlhttp.send();
     EOF
   }
@@ -17,5 +17,12 @@ describe Matchers::Ajax do
     end
 
     js_for("") { |js| js.should_not be_an_ajax }
+  end
+
+  it 'should identify parameters on call' do
+    js_for ajax_call do |js|
+      js.should be_an_ajax.with_parameters(:id => 20, :ba => "foo bar")
+      js.should_not be_an_ajax.with_params(:id => 20, :ba => "foo baz")
+    end
   end
 end
