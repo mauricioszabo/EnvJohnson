bdbfdaad78edfd04365b1102895f2cfecccaa07b
Maurício Szabo <mauricio.szabo@gmail.com>
2011-09-29 18:32:16 -0300

    Movidos códigos para arquivos que fazem mais sentido.

--- lib/env_johnson/matchers.rb
+++ lib/env_johnson/matchers.rb
@@ -14,52 +14,11 @@ end
 
 RSpec = Spec if Object.const_defined? :Spec
 
-module Matchers
-  class ReplaceInner
-    def initialize(element_id, params={})
-      @element_id = element_id
-      @params = params
-    end
-
-    def matches?(js)
-      @js = js
-      before = Nokogiri.parse @js.body_before
-      after = Nokogiri.parse @js.body
-      before = before.css("##{@element_id}").inner_html
-      after = after.css("##{@element_id}").inner_html
-      
-      conditions = [before != after]
-      conditions << (@params[:with] === after) if @params[:with]
-      conditions.all?
-    end
-
-    def with(string)
-      ReplaceInner.new @element_id, @params.merge(:with => string)
-    end
-
-    def failure_message
-      "Expected #{@js.inspect} #{rest}"
-    end
-    def negative_failure_message
-      "Expected #{@js.inspect} not to replace element '#{@element_id}'"
-    end
-
-    def rest
-      rest = "to replace element '#{@element_id}'"
-      rest << " with #{@params[:with]}" if @params[:with]
-      return rest
-    end
-    private :rest
-  end
-end
-
 module RSpec::Matchers
   def js_for(string, params={})
     js = SpecEnvJohnson.new string, params
     yield js
   end
-
-  def replace_inner_html_of(element_id)
-    Matchers::ReplaceInner.new(element_id)
-  end
 end
+
+require "env_johnson/matchers/replace_inner"
---
+++ lib/env_johnson/matchers/replace_inner.rb
@@ -0,0 +1,45 @@
+module Matchers
+  class ReplaceInner
+    def initialize(element_id, params={})
+      @element_id = element_id
+      @params = params
+    end
+
+    def matches?(js)
+      @js = js
+      before = Nokogiri.parse @js.body_before
+      after = Nokogiri.parse @js.body
+      before = before.css("##{@element_id}").inner_html
+      after = after.css("##{@element_id}").inner_html
+      
+      conditions = [before != after]
+      conditions << (@params[:with] === after) if @params[:with]
+      conditions.all?
+    end
+
+    def with(string)
+      ReplaceInner.new @element_id, @params.merge(:with => string)
+    end
+
+    def failure_message
+      "Expected #{@js.inspect} #{rest}"
+    end
+    def negative_failure_message
+      "Expected #{@js.inspect} not to replace element '#{@element_id}'"
+    end
+
+    def rest
+      rest = "to replace element '#{@element_id}'"
+      rest << " with #{@params[:with]}" if @params[:with]
+      return rest
+    end
+    private :rest
+  end
+end
+
+
+module RSpec::Matchers
+  def replace_inner_html_of(element_id)
+    Matchers::ReplaceInner.new(element_id)
+  end
+end
